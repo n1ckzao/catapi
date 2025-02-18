98c6db695261531b6c7450176a3347ae6baf681f
NICOLAS LIMA MEIRA <24122474@CFP127ALUNO.LOCAL>
2025-02-18 16:07:55 -0300

    api quase feita

---
+++ catapi.js
@@ -0,0 +1,26 @@
+'use strict'
+
+async function pesquisarFotos(){
+    const url = `https://api.thecatapi.com/v1/images/beng`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.message
+}
+
+function criarImagem(link){
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img')
+    novaImg.src = link
+
+    galeria.appendChild(novaImg)
+}
+
+async function preencherFotos(){
+    const fotos = await pesquisarFotos()
+    const galeria = document.getElementById('galeria')
+
+    galeria.replaceChildren('')
+    fotos.forEach(criarImagem)
+}
+
+preencherFotos()
